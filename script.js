af71f9042b8d7762e88815bcfd2a6667cdfc498c
majjeed <abdulmajeed.hersi@gmail.com>
2024-05-09 19:09:47 +0100

    Complete majority of game functionality
        1-populating field with 0's
        2-able to switch rounds
        3-can't take occupied spot
        4-print everything to console
    
        TODO:
        1- create a win condition
        2- update comments to reflect new code

--- script.js
+++ script.js
@@ -26,14 +26,117 @@ function Gameboard() {
 
     // In order to drop a token, we need to find what the lowest point of the
     // selected column is, *then* change that cell's value to the player number
-    const selectToken = (row, column, player) {
+    const dropToken = (row, column, player) => {
         // Our board's outermost array represents the row,
         // so we need to loop through the rows, starting at row 0,
         // find all the rows that don't have a token, then take the
         // last one, which will represent the bottom-most empty cell
-        const availableCells = board.filter
+        const availableCell = board[row][column].getValue();
+        //console.log('the value of this row/column is: ' + board[row][column].getValue());
+
         // why not just make a 2d array that has 3 rows and 3 columns
         // then pass in the data attribute of the row and column that was selected ???
+        // If no cells make it through the filter, 
+        // the move is invalid. Stop execution.        
+        if (availableCell === 1 || availableCell === 2) {
+            console.log('\n This Spot is already taken, try again...');
+            return false;
+        };
+        // Otherwise, I have a valid cell, the last one in the filtered array
+        board[row][column].addToken(player);
+        return true;
+    };
 
-    }
+    // This method will be used to print our board to the console.
+    // It is helpful to see what the board looks like after each turn as we play,
+    // but we won't need it after we build our UI
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    };
+
+    // Here, we provide an interface for the rest of our
+    // application to interact with the board
+    return { getBoard, dropToken, printBoard };
 }
+
+/*
+** A Cell represents one "square" on the board and can have one of
+** 0: no token is in the square,
+** 1: Player One's token,
+** 2: Player 2's token
+*/
+
+function Cell() {
+    let value = 0;
+
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+        value = player;
+    };
+
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+
+    return { addToken, getValue };
+}
+
+/* 
+** The GameController will be responsible for controlling the 
+** flow and state of the game's turns, as well as whether
+** anybody has won the game
+*/
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn`);
+    };
+
+    const playRound = (row, column) => {
+        // Drop a token for the current player
+        console.log(
+            `Dropping ${getActivePlayer().name}'s token into row ${row}, column ${column}...`
+        );
+
+        if (board.dropToken(row, column, getActivePlayer().token)) {
+            switchPlayerTurn();
+        }
+
+        /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+        // Switch player turn
+        printNewRound();
+    };
+
+    //Initial play game message
+    printNewRound();
+
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
